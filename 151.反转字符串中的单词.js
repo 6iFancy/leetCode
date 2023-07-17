5bf0c663900fdd4414b69271c240ae743df6b2f0
张留辉 <zhanglh@yngroups.com>
2023-07-17 15:07:34 +0800

    feat: create 151.反转字符串中的单词.js

---
+++ 151.反转字符串中的单词.js
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=151 lang=javascript
+ *
+ * [151] 反转字符串中的单词
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function (s) {
+  return s
+    .split(" ")
+    .filter((item) => item)
+    .reverse()
+    .join(" ");
+};
+// @lc code=end
